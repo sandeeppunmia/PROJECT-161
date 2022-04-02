e26b8745ab070082b50801d4dfa21ee4788e3403
sandeeppunmia <67103020+sandeeppunmia@users.noreply.github.com>
2022-04-02 22:43:40 +0530

    Add files via upload

---
+++ js/throw.js
@@ -0,0 +1,30 @@
+AFRAME.registerComponent("throw",{
+    init:function(){
+        this.throwBall();
+    },
+    throwBall:function(){
+        window.addEventListener("keydown",(e)=>{
+            if(e.key==="z"){
+                var ball=document.createElement("a-entity")
+                ball.setAttribute("geometry",{
+                    primitive:"sphere",
+                    radius:1
+                })
+                ball.setAttribute("material","color","black")
+                var cam = document.querySelector("#camera")
+                pos = cam.getAttribute("position")
+                ball.setAttribute("position",{
+                    x:pos.x,
+                    y:pos.y,
+                    z:pos.z
+                })
+                var camera = document.querySelector("#camera").object3D
+                var direction = new THREE.Vector3()
+                camera.getWorldDirection(direction)
+                ball.setAttribute("velocity",direction.multiplyScalar(-10))
+                var scene=document.querySelector("#scene")
+                scene.appendChild(ball)
+            }
+        })
+    }
+})
